1757c9bf7b016e48cbf04c4f7a988b9ded5b64ea
Peiyi Xu <xu_py311@outlook.com>
2024-02-12 20:47:18 -0500

    finished exercise 4.7

--- src/main/scala/fpinscala/exercises/errorhandling/Either.scala
+++ src/main/scala/fpinscala/exercises/errorhandling/Either.scala
@@ -8,18 +8,33 @@ enum Either[+E,+A]:
   case Left(get: E)
   case Right(get: A)
 
-  def map[B](f: A => B): Either[E, B] = ???
+  def map[B](f: A => B): Either[E, B] = this match
+    case Left(e) => Left(e)
+    case Right(a) => Right(f(a))
 
-  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = ???
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
 
-  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = ???
+  def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match
+    case Left(e) => b
+    case Right(a) => Right(a)
 
-  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = ???
+  def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] =
+    // this.flatMap(aa => b.map(bb => f(aa, bb)))
+    for
+      aa <- this
+      bb <- b
+    yield f(aa, bb)
+
 
 object Either:
-  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = ???
+  def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = es match
+    case Nil => Right(Nil)
+    case h::t => f(h).map2(traverse(t)(f))(_ :: _) //f(h).flatMap(hh => traverse(t)(f).map(hh::_))
 
-  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = ???
+  def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] =
+    traverse(es)(x => x)
 
   def mean(xs: IndexedSeq[Double]): Either[String, Double] = 
     if xs.isEmpty then
